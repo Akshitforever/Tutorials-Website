c6d3fce6f3af61d9136f48be127faa59cde7a1dd
Akshit Bansal <akshitbansal2828@gmail.com>
2019-02-04 08:36:02 +0530

    Page added for Video
    -Card Page minor
    -Sign Up added

---
+++ src/js/prev.js
@@ -0,0 +1,8 @@
+function showLogin(){
+    document.querySelector('.login').style.display='block';
+    document.querySelector('.sign-up').style.display='none';    
+}
+function showSignUp(){
+    document.querySelector('.login').style.display='none';
+    document.querySelector('.sign-up').style.display='block';    
+}
